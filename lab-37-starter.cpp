d0b2a7b42aae988c2d14d7087f91cfacdf12808d
Jed Aficial's PC <jedaficial1@gmail.com>
2024-12-08 02:00:57 -0800

    10-minute update

--- lab-37-starter.cpp
+++ lab-37-starter.cpp
@@ -1,7 +1,28 @@
+// COMSC-210 | Jed Aficial | Lab 37
+// github link: https://github.com/jaficial/210-lab-37.git
+
 #include <iostream>
+#include <string>
+#include <map>
 using namespace std;
 
+int sum_ascii(string);
 
+int sum_ascii(string string_data){
+    int string_length = string_data.length();
+    int ascii_total = 0;
+    for (int i = 0; i < string_length; i++){
+        ascii_total = ascii_total + (int) string_data[i];
+    }
+
+    return ascii_total;
+}
+/*
+    THINGS TO DO: 
+    PART 1: "sum_ascii" receives a single string and returns the 
+             sum of the string's ASCII values
+             - do so by type casting a char into an int 
+*/
 int main() {
     char a = 'A';
     cout << a << endl;
@@ -10,6 +31,10 @@ int main() {
     cout << b << endl;
     cout << (char) b << endl;
     
+    string test = "A";
+    int total = sum_ascii(test);
+
+    cout << "This is the total of string_data: " << total << endl;
 
     return 0;
 }
